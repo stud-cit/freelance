e3a9388b514513cab3a201d9e8219015c4cc498a
Andrey <gorand.one@gmail.com>
2019-07-31 22:44:28 +0300

    verification mail + edit migration

--- database/migrations/2019_07_26_000001_create_users_table.php
+++ database/migrations/2019_07_26_000001_create_users_table.php
@@ -14,8 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('name', 45);
+            $table->string('surname', 45);
+            $table->string('patronymic', 45)->nullable();
+            $table->date('birthday_date')->nullable();
+            $table->string('phone_number', 45)->nullable();
+            $table->string('avatar', 255)->nullable();
+            $table->bigInteger('id_role')->unsigned();
+            $table->foreign('id_role')->references('id_role')->on('roles');
+            $table->text('about_me')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
