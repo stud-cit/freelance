3abe39b1d099b1a8852db477cfdfbec3d9be62a0
Maxim <kipisham@gmail.com>
2019-08-05 11:31:59 +0300

    fix

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -14,9 +14,13 @@
                 @else
                 <div class="col-1 offset-7 mt-1 small"></div>
                 @endif
+                @if(strpos(url()->current(), 'login') === false)
                 <div class="col-1">
                     <a href="{{ route('login') }}" class="badge-pill bg-light py-1 px-3 text-dark small">Вхід</a>
                 </div>
+                @else
+                <div class="col-1"></div>
+                @endif
                 <div class="col-1 mt-1">
                     UA
                 </div>
