af7bc90fa0dda0a48dd6ae3ce4c48f25bb279129
Nekrowiz <alex.shut97@gmail.com>
2019-08-07 16:31:32 +0300

    order page start+

--- resources/views/orders/order.blade.php
+++ resources/views/orders/order.blade.php
@@ -9,15 +9,19 @@
 <div class="container">
     <div class="row">
         <div class="col-9">
-            <a href="{{}}" class="btn">Пошук</a>
+            <a href="/orders" class="btn">Пошук</a>
         </div>
-        <div class="col-9" style="background-color: #0076de">
-            <div class="container text-white">
+        <div class="col-9 text-white" style="background-color: #0076de">
+            <div class="container">
                 <div class="row">
                     <div class="col-3 offset-1">Shop</div>
-                    <div class="col-1 offset-2">Ціна:</div>
+                    <div class="col-1 offset-5">Ціна:</div>
                     <div class="col-1">132</div>
                 </div>
+            </div>
+            <div class="tags">#full #blah</div>
+            <div class="description">
+
             </div>
         </div>
     </div>
