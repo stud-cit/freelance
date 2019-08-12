d68cb6e560686345cbbb0c5d977deec3309c54ea
Maxim <kipisham@gmail.com>
2019-08-12 11:15:51 +0300

    proposals save

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Auth::routes(['verify' => true]);
 
 Route::get('/orders', 'OrdersController@index');
-Route::get('/orders/{id}', 'OrdersController@order')->middleware(\App\Http\Middleware\LoginCheck::Class);
+Route::get('/orders/{id}', 'OrdersController@order')->middleware('logged.in');
+Route::post('/orders/{id}', 'OrdersController@add_proposal')->name('add_proposal');
 
 Route::get('/customers', 'UsersController@customers');
 Route::get('/workers', 'UsersController@workers');
 
 Route::get('/', 'HomeController@index');
 
-Route::get('/admin', 'AdminController@index')->middleware(\App\Http\Middleware\AdminCheck::class);
+Route::get('/admin', 'AdminController@index')->middleware('is.admin');
