20b73452468e4a1ab1266c77b2d4349f419d8b6a
ftfd <ftfd.panchenko@gmail.com>
2019-08-28 14:25:27 +0300

    order switch fix

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -37,7 +37,7 @@
                     <div class="d-flex flex-row mb-3 mt-2" id="new_order-toggle">
                         <div class="col-11 pt-3 shadow-lg" style="height: 60px; position: relative;">Створити власний проект</div>
                         <div class="col-1">
-                            <div class="circle text-center text-white font-weight-bold bg-blue square-60 circle min-width-60" style="font-size: 36px; position: absolute; left: -15px;">&#43;</div>
+                            <div class="circle text-center text-white font-weight-bold bg-blue square-60 circle min-width-60" style="font-size: 36px; position: absolute; left: -15px;" onclick="$(this).text($('#new-order').css('display') == 'none' ? '-' : '+')">&#43;</div>
                         </div>
                     </div>
                 </div>
