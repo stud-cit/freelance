d4546cff5e93739b4de4489edcb51d7ee015a4f1
Nekrowiz <alex.shut97@gmail.com>
2019-09-09 14:41:40 +0300

    fix

--- resources/views/users/workers.blade.php
+++ resources/views/users/workers.blade.php
@@ -11,19 +11,21 @@
         <div class="col-9">
             <div class="font-weight-bold font-size-18">Всі виконавці</div>
             <div class="container">
-                @foreach($workers as $list)
+                @foreach($data as $list)
                     <div class="d-flex flex-row mb-3 mt-2 pointer to-profile">
                         <div class="col-1 px-0 min-width-70" data-id="{{$list->id_user}}">
                             <img src="{{$list->avatar}}" class="mt-1 square-60 avatar square">
                         </div>
                         <div class="col-9 shadow bg-white" data-id="{{$list->id_user}}">
                             <div class="flex-row">
-                                <div class="font-weight-bold mt-2"><span class="" data-id="{{$list->id_user}}">{{$list->name}} {{$list->surname}}</span></div>
+                                <div class="font-weight-bold font-size-18 mt-2"><span class="" data-id="{{$list->id_user}}">{{$list->name}} {{$list->surname}}</span></div>
                                 <div class="tag-list">
-                                    @foreach($workers->categories as $tags)
-                                        <span class="tags font-italic font-size-10">{{$tags->name}}</span>
-                                    @endforeach
+                                    <!--foreach($list->categories as $tags)
+                                        <span class="tags font-italic font-size-10">$tags->name</span>
+                                    endforeach-->
                                 </div>
+                                <div>{{strlen($list->about_me) > 50 ? substr($list->about_me, 0, 50) . '...' : $list->about_me}}</div>
+                                <div class="text-right font-size-10">Дата реєстрації: {{$list->created_at}}</div>
                             </div>
                         </div>
                     </div>
