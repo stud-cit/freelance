c9d9995d084565ca3312840ce3caca8302aa82a8
ftfd <ftfd.panchenko@gmail.com>
2019-10-10 13:48:19 +0300

    footer updatet

--- resources/views/site/footer.blade.php
+++ resources/views/site/footer.blade.php
@@ -1,38 +1,32 @@
 @section('footer')
-    <div class="container-fluid bg-secondary">
+    <div class="container-fluid bg-secondary text-white">
         <div class="row py-2 text-white">
             <div class="col-2 offset-1">
-                <p class="text-capitalize">Общая информация</p>
+                <p class="text-capitalize">Загальна інформація</p>
                 <hr style="border-color: white">
-                    <a>Главная</a><br>
-                    <a>О нас</a><br>
-                    <a>Правила сайта</a><br>
-                    <a>Ещё что-то</a><br>
-                    <a>Ну и ещё</a>
+                    <a href="/" class="text-white">Головна</a><br>
+                    <a>Про нас</a><br>
+                    <a>Правила сайту</a>
             </div>
-            <div class="col-2 offset-1">
-                <p class="text-capitalize">Общая информация</p>
-                <hr style="border-color: white">
-                <a>Главная</a><br>
-                <a>О нас</a><br>
-                <a>Правила сайта</a><br>
-                <a>Ещё что-то</a><br>
-                <a>Ну и ещё</a>
-            </div>
+
             <div class="col-2 offset-1">
                 <p class="text-capitalize">Аккаунт</p>
                 <hr style="border-color: white">
-                <a>Мой аккаунт</a><br>
-                <a>Вход</a><br>
-                <a>Регистрация</a><br>
-                <a>Ещё что-то</a>
+                <a href="{{ route('profile') }}" class="text-white">Мій аккаунт</a><br>
+                <a href="{{ route('login') }}" class="text-white">Вхід</a>
             </div>
             <div class="col-2 offset-1">
-                <p class="text-capitalize">Социальные сети</p>
+                <p class="text-capitalize">Соціальні мережі</p>
                 <hr style="border-color: white">
                 <a>Facebook</a><br>
                 <a>Twitter</a><br>
                 <a>Instagram</a>
+            </div>
+            <div class="col-2 offset-1">
+                <p class="text-capitalize">Розробка</p>
+                <hr style="border-color: white">
+                <a>Студентський центр інформаційних технологій</a><br>
+                <a href="mailto:s.panchenko@ias.sumdu.edu.ua" class="text-white">s.panchenko</a>
             </div>
         </div>
     </div>
