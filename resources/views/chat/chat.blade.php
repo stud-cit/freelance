bc06ef291a143134a368be5a9c29335dbb817212
ftfd <ftfd.panchenko@gmail.com>
2019-10-16 12:22:57 +0300

    chat front

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -5,7 +5,39 @@
 @endsection
 
 @section('content')
-
+<div class="d-flex h-100">
+        <div class="flex-column bg-success w-25" style="overflow-y: scroll">
+            @for($i=0; $i< 30; $i++)
+            <div>
+                luke, a'm your friend
+            </div>
+            @endfor
+        </div>
+        <div class="bg-danger w-75 h-100">
+            <div class="w-100">
+                <form action="send_msg">
+                    <div class="input-group">
+                        <input type="text" class="form-control col-11" name="msg">
+                        <button class="form-control bg-blue text-white col-1"> > </button>
+                    </div>
+                </form>
+            </div>
+            <div class="d-flex flex-column " style="overflow-y: scroll">
+                @for($i=0; $i< 30; $i++)
+                    <div class="flex-row">
+                        <div class="float-left bg-primary m-2 p-2">
+                            III!
+                        </div>
+                    </div>
+                    <div class="flex-row">
+                        <div class="float-right bg-primary m-2 p-2">
+                            YOUUU!
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+</div>
 @endsection
 
 @section('footer')
