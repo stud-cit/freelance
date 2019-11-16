96bf0dddf6bb5e20123550f772faabcbd55af678
ftfd <ftfd.panchenko@gmail.com>
2019-11-16 19:20:00 +0200

    login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,23 +7,27 @@
 @section('content')
     <div class="container">
         <div class="row">
-
-            <div class="col-5 container">
+            <div class="col-10 offset-1 shadow-lg bg-light-grey p-4">
                 <div class="row">
-                    <div class="col-12 font-weight-bold text">Авторизація</div>
-                </div>
-                <div class="row">
-                    <div class="col-10 offset-1 font-italic small">Почніть співробітництво</div>
-                </div>
-                <div>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <ul class="list-group">
-                            <li class="list-group-item d-flex flex-row">
+                    <div class="col-6 d-flex border-right border-grey">
+                        <div class="d-flex flex-column flex-shrink-1 align-self-center mx-auto text-white col-6">
+                            <p>Як зареєструватися?</p>
+                            <p>1. <a href="">Зв'язатися</a> з нами</p>
+                            <p>2. Дочекатися даних для входу на свою електронну адресу</p>
+                            <p>3. Використати дану форму для авторизації</p>
+                        </div>
+                    </div>
+                    <div class="col-6 text-white">
+                        <div class="row">
+                            <div class="col-12 font-weight-bold font-size-50">Авторизація</div>
+                        </div>
+                        <div>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
                                 <div class="">&nbsp;</div>
                                 <div class="d-flex flex-column">
                                     <label for="name" class="col-form-label">Електронна адреса</label>
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror border-0" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="email">
+                                    <input id="email" type="email" class="form-control bg-deep-dark text-grey @error('email') is-invalid @enderror border-0" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="email">
 
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -32,14 +36,12 @@
                                     @enderror
                                 </div>
                                 <div class="">&nbsp;</div>
-                            </li>
 
-                            <li class="list-group-item d-flex flex-row">
                                 <div class="">&nbsp;</div>
                                 <div class="d-flex flex-column">
                                     <label for="name" class="col-form-label">Пароль</label>
 
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror border-0" name="password" required autocomplete="new-password" placeholder="********">
+                                    <input id="password" type="password" class="form-control bg-deep-dark text-grey @error('password') is-invalid @enderror border-0" name="password" required autocomplete="new-password" placeholder="********">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -48,38 +50,35 @@
                                     @enderror
                                 </div>
                                 <div class="">&nbsp;</div>
-                            </li>
-                        </ul>
 
-                        <div class="row mt-4">
-                            <div class="col-lg-4 col-12 offset-1 custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input pointer" id="rememberme" name="remember">
-                                <label for="rememberme" class="custom-control-label pointer">Запам'ятати</label>
-                            </div>
-                            <div class="col-lg-5 col-12 offset-lg-2">
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        Забули пароль?
-                                    </a>
-                                @endif
-                            </div>
+                                <div class="row mt-4">
+                                    <div class="col-lg-4 col-12 offset-1 custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input pointer" id="rememberme" name="remember">
+                                        <label for="rememberme" class="custom-control-label pointer">Запам'ятати</label>
+                                    </div>
+                                    <div class="col-lg-5 col-12 offset-lg-2">
+                                        @if (Route::has('password.request'))
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                Забули пароль?
+                                            </a>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mt-5">
+                                    <div class="col-lg-5 col-12">
+                                        <button type="submit" class="btn text-white badge-pill w-100 bg-orange">
+                                            Вхід
+                                        </button>
+                                    </div>
+
+                                    <div class="col-lg-5 col-12">
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="form-group row mt-5">
-                            <div class="col-lg-5 col-12">
-                                <button type="submit" class="btn text-white badge-pill w-100 bg-violet">
-                                    Вхід
-                                </button>
-                            </div>
-
-                            <div class="col-lg-5 col-12">
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
-            <div class="col-6 offset-1">
-                <img src="{{ asset('login.jpg') }}" class="img-fluid">
             </div>
         </div>
     </div>
